fb27241791d5b8f563ffc17647c60cc62c6c56b0
patrezelopes <patrezelopes@gmail.com>
2021-06-28 02:54:31 -0300

    Update settings.py
    allowed hosts *

--- cnabcoders/cnabcoders/settings.py
+++ cnabcoders/cnabcoders/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jowqq4&nw1f^$aam66ghfifvbuebrq1^a_e1x5)do-z(mqr@93
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
